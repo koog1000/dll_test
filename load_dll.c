1841a265c082ad11d3cd73551a73239cc3cb1e37
Keenan Kugler <keenankugler@gmail.com>
2019-06-16 11:15:28 -0700

    cleanup print statements in load_dll.c

--- load_dll.c
+++ load_dll.c
@@ -6,13 +6,11 @@ int main(){
     HANDLE ldll;
     int (*add2)(int);
     int (*mul)(int,int);
-        printf("ERROR with code: %d\n", GetLastError());
 
     ldll = LoadLibrary("test.dll");
-    printf("Load returned: %d", &ldll);
-        printf("ERROR with code: %d\n", GetLastError());
     if(ldll>(void*)HINSTANCE_ERROR)
     {
+        printf("Load DLL success. Returned: %d\n", &ldll);
         add2 = GetProcAddress(ldll, "add2");
         mul = GetProcAddress(ldll, "mul");
         printf("add2(3): %d\nmul(4,5): %d\n", add2(3), mul(4,5));
