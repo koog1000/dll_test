9a3649ff5847b0186e030fdc5402b881200a4674
Keenan Kugler <keenankugler@gmail.com>
2019-06-16 15:04:09 -0700

    add structs and getter\setter for struct

--- test_dll.c
+++ test_dll.c
@@ -1,23 +1,50 @@
-#ifdef BUILD_DLL
-#define EXPORT __declspec(dllexport)
-#define _CALL_ __stdcall
-#else
-#ifdef BUILD_STATIC
+
+
 #include "test_dll.h"
-#define EXPORT 
-#define _CALL_ 
-#else
-#define EXPORT __declspec(dllimport)
-#define _CALL_ __stdcall
-#endif //BUILD_STATIC
-#endif //BUILD_DLL
+
+typedef struct reg_map
+{
+    char name[255];
+    int value;
+} reg_map_t;
+
+reg_map_t reg[2] = {
+    {"Test1", 56},
+    {"Test2", 123}
+};
+
+EXPORT int _CALL_ get_val(char* string, int* value)
+{
+    for(int i=0; i < (sizeof(reg)/sizeof(reg_map_t)); i++)
+    {
+        if (strcmp(reg[i].name, string) == 0)
+        {
+            *value = reg[i].value;
+            return 0;
+        }
+    }
+    return -1;
+}
+
+EXPORT int _CALL_ set_val(char* string, int value)
+{
+    for(int i=0; i < (sizeof(reg)/sizeof(reg_map_t)); i++)
+    {
+        if (strcmp(reg[i].name, string) == 0)
+        {
+            reg[i].value = value;
+            return 0;
+        }
+    }
+    return -1;
+}
 
 EXPORT int _CALL_ add2(int num){
-  return num + 2;
+    return num + 2;
 }
 
 EXPORT int _CALL_ mul(int num1, int num2){
-  return num1 * num2;
+    return num1 * num2;
 }
 
 EXPORT int _CALL_ incr_by_ref(int* num)
--- test_dll.h
+++ test_dll.h
@@ -1,12 +1,27 @@
 #ifndef TEST_DLL_H
 #define TEST_DLL_H
 
-// EXPORT int mul(int, int);
-// EXPORT int add2(int);
-// EXPORT int incr_by_ref(int*, int);
-int mul(int, int);
-int add2(int);
-int incr_by_ref(int*);
-int incr_by_ref_loop(int*, int);
+    #ifdef BUILD_DLL
+        #define EXPORT __declspec(dllexport)
+        #define _CALL_ __stdcall
+    #else
+        #ifdef BUILD_STATIC
+            #define EXPORT 
+            #define _CALL_ 
+        #else
+            #define EXPORT __declspec(dllimport)
+            #define _CALL_ __stdcall
+        #endif //BUILD_STATIC
+    #endif //BUILD_DLL
+
+    #include <string.h>
+    #include <stdio.h>
+
+    EXPORT int _CALL_ mul(int, int);
+    EXPORT int _CALL_ add2(int);
+    EXPORT int _CALL_ incr_by_ref(int*);
+    EXPORT int _CALL_ incr_by_ref_loop(int*, int);
+    EXPORT int _CALL_ get_val(char*, int*);
+    EXPORT int _CALL_ set_val(char*, int);
 
 #endif // TEST_DLL_H
